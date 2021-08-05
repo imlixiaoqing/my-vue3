e6be2bfd578df25fe065622f17cb1afc5854a723
李小庆 <lixiaoqing0126@outlook.com>
2021-08-05 15:33:50 +0800

    vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,48 @@
+// 该对象将会被 webpack-merge 合并入最终的 webpack 配置
+
+module.exports = {
+  // 简单的配置方式
+  // configureWebpack: config => {
+  //   if (process.env.NODE_ENV === 'production') {
+  //     // 为生产环境修改配置...
+  //   } else {
+  //     // 为开发环境修改配置...
+  //   }
+  // },
+  // 链式操作 (高级)
+  // chainWebpack: config => {
+    // const svgRule = config.module.rule('svg')
+
+    // // 清除已有的所有 loader。
+    // // 如果你不这样做，接下来的 loader 会附加在该规则现有的 loader 之后。
+    // svgRule.uses.clear()
+
+    // // 添加要替换的 loader
+    // svgRule
+    //   .use('vue-svg-loader')
+    //     .loader('vue-svg-loader')
+
+    // config.module
+    //   .rule('vue')
+    //   .use('vue-loader')
+    //     .tap(options => {
+    //       // 修改它的选项...
+    //       return options
+    //     })
+    //   .rule('graphql')
+    //   .test(/\.graphql$/)
+    //   .use('graphql-tag/loader')
+    //     .loader('graphql-tag/loader')
+    //     .end()
+    //   // 你还可以再添加一个 loader
+    //   .use('other-loader')
+    //     .loader('other-loader')
+    //     .end()
+
+    // config
+    //   .plugin('html')
+    //   .tap(args => {
+    //     return [/* 传递给 html-webpack-plugin's 构造函数的新参数 */]
+    //   })
+  // }
+}
