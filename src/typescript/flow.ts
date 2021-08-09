911a88c96f3105bba9870b7fa89290704ec7713e
李小庆 <lixiaoqing0126@outlook.com>
2021-08-09 22:03:04 +0800

    typescript

---
+++ src/typescript/flow.ts
@@ -0,0 +1,7 @@
+function sum(a: number, b: number) {
+  console.log(a + b)
+}
+
+sum(100, 200)
+
+sum(100, '100')
---
+++ src/typescript/weakly.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+  console.log(a + b)
+}
+
+sum(100, 200)
+
+sum(100, '100')
