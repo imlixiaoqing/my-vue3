07aaca4ec932f45faa2a942ef4ccf59615e472ac
李小庆 <lixiaoqing0126@outlook.com>
2021-08-10 16:49:43 +0800

    mapbox

---
+++ src/views/Map.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="mapId" class="map-container"></div>
+</template>
+
+<script lang="ts">
+import 'mapbox-gl/dist/mapbox-gl.css'
+import mapboxgl from 'mapbox-gl'
+mapboxgl.accessToken = 'pk.eyJ1IjoiMTY2MTk3ODQ4NDUiLCJhIjoiY2tydnI5bmZuMDliYTJvbjBnZDdlZHRkMiJ9.ayzPii_6fVacLh5Jjgh4uw'
+export default {
+  components: {
+  },
+  data() {
+    return {
+    };
+  },
+
+  created() {
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+
+.map-container {
+  width: 100%;
+  height: 100%;
+  background: red;
+}
+
+</style>
