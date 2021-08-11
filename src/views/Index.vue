448eeb5ea6e9afd2ae807c7947ab61242615c615
李小庆 <lixiaoqing0126@outlook.com>
2021-08-11 13:40:39 +0800

    3D 地形

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -18,10 +18,37 @@ export default {
     initMap() {
       const map = new mapboxgl.Map({
         container: "map", // container ID
-        style: "mapbox://styles/mapbox/streets-v11", // style URL
-        center: [-74.5, 40], // starting position [lng, lat]
-        zoom: 9, // starting zoom
+        style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y',
+        zoom: 13.1,
+        center: [-114.34411, 32.6141],
+        pitch: 85,
+        bearing: 80,
       });
+
+      map.on('load', () => {
+        map.addSource('mapbox-dem', {
+          'type': 'raster-dem',
+          'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+          'tileSize': 512,
+          'maxzoom': 14
+        });
+        // add the DEM source as a terrain layer with exaggerated height
+        map.setTerrain({
+          'source': 'mapbox-dem',
+          'exaggeration': 1.5
+        });
+
+        // add a sky layer that will show when the map is highly pitched
+        map.addLayer({
+          'id': 'sky',
+          'type': 'sky',
+          'paint': {
+            'sky-type': 'atmosphere',
+            'sky-atmosphere-sun': [0.0, 0.0],
+            'sky-atmosphere-sun-intensity': 15
+          }
+        });
+      })
 
       const coordinatesGeocoder = function(query) {
         // Match anything which looks like
