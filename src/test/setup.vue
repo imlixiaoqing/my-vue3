0111ea3f8222d021a60806ba8f10ae2befb8dc98
李小庆 <lixiaoqing0126@outlook.com>
2021-08-16 20:51:47 +0800

    setup

---
+++ src/test/setup.vue
@@ -0,0 +1,31 @@
+<template>
+<h1>{{name}}{{age}}</h1>
+<button @click="say('nihao')">say</button>
+</template>
+
+<script lang='ts'>
+import { h } from 'vue'
+export default {
+  setup() {
+    // let name = 'lxq'
+    // let age = 18
+
+    // function say(other:String) {
+    //   alert(other)
+    // }
+
+    // return {
+    //   name,
+    //   age,
+    //   say
+    // }
+
+    return () => h('h1', 'hhh')
+    
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
