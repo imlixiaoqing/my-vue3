264288624cde248db1e1afe82a8ceb653f0563c7
李小庆 <lixiaoqing0126@outlook.com>
2021-09-18 11:23:09 +0800

    PubSub

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,28 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import PubSub from 'pubsub-js'
+export default {
+  data() {
+    return {
+      token: ''
+    }
+  },
+  methods: {
+    mySubscriber(msg, data) {
+      console.log('lxq')
+      console.log( msg, data );
+    },
+
+  },
+  mounted() {
+    this.token = PubSub.subscribe('MY TOPIC', this.mySubscriber);
+  },
+  beforeUnmount() {
+    PubSub.unsubscribe(this.token);
+  }
+}
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,22 +3,32 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <SvgIcon iconClass="icon-ditulei-ditumarker-juhedian"/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="myPublish">发布消息</button>
+    <About/>
   </div>
 </template>
 
 <script>
+import PubSub from 'pubsub-js'
 // @ is an alias to /src
 import { myMixin } from '@/mixin/mixin'
 import HelloWorld from '@/components/HelloWorld.vue'
+import About from './About'
 
 export default {
   name: 'Home',
   mixins: [myMixin],
   components: {
-    HelloWorld
+    HelloWorld,
+    About
+  },
+  methods: {
+    myPublish() {
+      PubSub.publish('MY TOPIC', 'hello world!');
+    }
   },
   created() {
-    console.log('组件钩子被调用')
-  }
+    // console.log('组件钩子被调用')
+  },
 }
 </script>
